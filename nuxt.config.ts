8af45d7f4235e118c2c4d73b841aec9eb3ad2ca3
Chritch <chritch-7@outlook.com>
2024-03-13 10:05:04 +0800

    fix: enable website icon replacement

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    }
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   modules: [
